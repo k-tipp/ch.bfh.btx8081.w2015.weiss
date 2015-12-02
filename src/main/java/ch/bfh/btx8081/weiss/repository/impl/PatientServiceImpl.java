b9fe43a3ab88821a3a9f7e3531f28b9c51c4c6aa
wyslu1 <wyslu1@gmail.com>
2015-12-02 22:04:24 +0100

    added PatientServiceImpl-Class for Patient-persistence

--- src/main/java/ch/bfh/btx8081/weiss/repository/impl/PatientServiceImpl.java
+++ src/main/java/ch/bfh/btx8081/weiss/repository/impl/PatientServiceImpl.java
@@ -1,17 +1,47 @@
 package ch.bfh.btx8081.weiss.repository.impl;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.persistence.EntityManager;
 
 import ch.bfh.btx8081.weiss.model.Patient;
 import ch.bfh.btx8081.weiss.repository.PatientService;
 
 public class PatientServiceImpl implements PatientService {
 
+	protected EntityManager em;
+
+	public PatientServiceImpl(EntityManager em) {
+		this.em = em;
+	}
+
 	@Override
 	public Patient create(Patient patient) {
-		// TODO Auto-generated method stub
-		return null;
+		Patient emp = patient;
+		return emp;
 	}
+	
+	public Patient create(int id, String firstName, String lastName, String gender, String street, String ssn, LocalDate birthday, String city, String zip, String eMail, String picture, int harmID) {
+		// TODO Auto-generated method stub
+		Patient emp = new Patient(id);
+		emp.setBirthday(birthday);
+		emp.setCity(city);
+		emp.seteMail(eMail);
+		emp.setFirstName(firstName);
+		emp.setGender(gender);
+		emp.setHarmID(harmID);
+		emp.setLastName(lastName);
+		emp.setPicture(picture);
+		emp.setSsn(ssn);
+		emp.setStreet(street);
+		emp.setZip(zip);
+		em.getTransaction().begin();
+		em.persist(emp);
+		em.getTransaction().commit();
+		return emp;
+	}
+
 
 	@Override
 	public Patient update(Patient patient) {
@@ -33,8 +63,7 @@ public class PatientServiceImpl implements PatientService {
 
 	@Override
 	public Patient getPatientById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.find(Patient.class, id);
 	}
 
 }
