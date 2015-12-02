a1cef73f71f4eaf71d416a8bc527711e1f4ac98a
wyslu1 <wyslu1@gmail.com>
2015-12-02 22:33:24 +0100

    You can see and click lots of things but it still doesn't quite work

--- src/main/java/ch/bfh/btx8081/weiss/view/PatientViewImpl.java
+++ src/main/java/ch/bfh/btx8081/weiss/view/PatientViewImpl.java
@@ -3,6 +3,12 @@ package ch.bfh.btx8081.weiss.view;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+
+import ch.bfh.btx8081.weiss.repository.impl.MssqlEntityManager;
+import ch.bfh.btx8081.weiss.repository.impl.PatientServiceImpl;
 
 public class PatientViewImpl extends PatientView implements View {
 
@@ -11,6 +17,14 @@ public class PatientViewImpl extends PatientView implements View {
 	
 	public PatientViewImpl(final Navigator navigator) {
 		super();
+		this.btn_patient.addClickListener(new Button.ClickListener() {
+	
+			@Override
+			public void buttonClick(ClickEvent event) {
+				PatientServiceImpl psi = new PatientServiceImpl(MssqlEntityManager.createEntityManager());
+				patient_name.setValue(psi.getPatientById(1).getFirstName()+" "+psi.getPatientById(1).getLastName());
+			}
+		});
 	}
 
 	public PatientViewImpl() {
