ff54c39864a2c023da0d361129696f7ba94ec4e2
k-tipp <k-tipp@users.noreply.github.com>
2015-12-02 21:46:32 +0100

    Added PatientView as default page

--- src/main/java/ch/bfh/btx8081/weiss/application/MyUI.java
+++ src/main/java/ch/bfh/btx8081/weiss/application/MyUI.java
@@ -5,55 +5,29 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
-import ch.bfh.btx8081.weiss.model.PatientModel;
+import ch.bfh.btx8081.weiss.view.PatientViewImpl;
 
 /**
  *
  */
 @Theme("mytheme")
-@Widgetset("ch.bfh.btx8081.weiss.Shrinkie.MyAppWidgetset")
+@Widgetset("ch.bfh.btx8081.weiss.view.MyAppWidgetset")
 public class MyUI extends UI {
-	PatientModel pm = null;
-
-	@Override
-	protected void init(VaadinRequest vaadinRequest) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(layout);
-
-		Button button = new Button("Create Patientmodel");
-		button.addClickListener(new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				// layout.addComponent(new Label("Fuck off if this doesn't
-				// work!"));
-				pm = new PatientModel();
-
-			}
-		});
-		Button secondButton = new Button("get House!");
-		secondButton.addClickListener(new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				// layout.addComponent(new Label("Fuck off if this doesn't
-				// work!"));
-
-				layout.addComponent(new Label(pm.getPatient().getFirstName()));
-
-			}
-		});
-
-		layout.addComponent(button);
-		layout.addComponent(secondButton);
-	}
+	
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        Responsive.makeResponsive(this);
+        setLocale(vaadinRequest.getLocale());
+        getPage().setTitle("Home");
+        
+        setContent(new PatientViewImpl());
+        
+    }
 
 	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
 	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
