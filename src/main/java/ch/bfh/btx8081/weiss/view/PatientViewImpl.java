fd998d062cdbf1f9a7758a755cae7a4082a5c683
k-tipp <k-tipp@users.noreply.github.com>
2015-12-02 21:33:29 +0100

    Added/Updated PatientView and PatientViewImpl

---
+++ src/main/java/ch/bfh/btx8081/weiss/view/PatientViewImpl.java
@@ -0,0 +1,25 @@
+package ch.bfh.btx8081.weiss.view;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+public class PatientViewImpl extends PatientView implements View {
+
+	private static final long serialVersionUID = 1L;
+	public static final String VIEW_NAME = "PatientView";
+	
+	public PatientViewImpl(final Navigator navigator) {
+		super();
+	}
+
+	public PatientViewImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+}
