b8e2d5bde5cd2a55d7fcad983329b46dfe8df054
wyslu1 <wyslu1@gmail.com>
2015-12-16 15:24:49 +0100

    solved problem with updating harmlevel when returning to Patientoverview

--- src/main/java/ch/bfh/btx8081/weiss/view/PatientOverviewImpl.java
+++ src/main/java/ch/bfh/btx8081/weiss/view/PatientOverviewImpl.java
@@ -8,17 +8,17 @@ import ch.bfh.btx8081.weiss.model.Patient;
 import ch.bfh.btx8081.weiss.repository.DatabaseHandler;
 import ch.bfh.btx8081.weiss.repository.PatientService;
 
-
 /**
  * The Class PatientOverviewImpl controls the patient overview user interface.
  */
-public class PatientOverviewImpl extends PatientOverview  implements View {
+public class PatientOverviewImpl extends PatientOverview implements View {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
-	
+
 	/** The Constant VIEW_NAME of the patient overview. */
 	public static final String VIEW_NAME = "PatientOverview";
+	private Navigator navigator;
 
 	/**
 	 * Instantiates a new patient overview implementation.
@@ -28,21 +28,26 @@ public class PatientOverviewImpl extends PatientOverview  implements View {
 	 */
 	public PatientOverviewImpl(Navigator navigator) {
 		super();
-		for(Patient p:DatabaseHandler.patientService.getAllPatients())
-		{
-		   PatientDetailComponentImpl pdc = new PatientDetailComponentImpl(p, navigator);
-		   addComponent(pdc);
-		}
+		this.navigator = navigator;
+
 	}
 
-	/* (non-Javadoc)
-	 * @see com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener.ViewChangeEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener.
+	 * ViewChangeEvent)
 	 */
 	@Override
 	public void enter(ViewChangeEvent event) {
-//	       new PatientGenerator(); 
-//	       new MedicationGenerator();
-		
+		// new PatientGenerator();
+		// new MedicationGenerator();
+		removeAllComponents();
+		for (Patient p : DatabaseHandler.patientService.getAllPatients()) {
+			PatientDetailComponentImpl pdc = new PatientDetailComponentImpl(p, navigator);
+			addComponent(pdc);
+		}
 
 	}
 }
