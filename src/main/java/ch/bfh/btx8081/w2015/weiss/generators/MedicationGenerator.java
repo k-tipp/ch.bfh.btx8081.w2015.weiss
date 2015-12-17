57a88a5576b470491c6fff4bac1c76f856403f60
k-tipp <k-tipp@users.noreply.github.com>
2015-12-17 02:51:49 +0100

    Updated MedicationGenerator

--- src/main/java/ch/bfh/btx8081/w2015/weiss/generators/MedicationGenerator.java
+++ src/main/java/ch/bfh/btx8081/w2015/weiss/generators/MedicationGenerator.java
@@ -2,6 +2,7 @@ package ch.bfh.btx8081.w2015.weiss.generators;
 
 import java.time.LocalDateTime;
 
+import ch.bfh.btx8081.weiss.model.Drug;
 import ch.bfh.btx8081.weiss.repository.DatabaseHandler;
 import ch.bfh.btx8081.weiss.repository.MedicationService;
 
@@ -15,9 +16,10 @@ public class MedicationGenerator {
 	 */
 	public MedicationGenerator() {
 		MedicationService ms = DatabaseHandler.medicationService;
-
+		Drug drug = DatabaseHandler.drugService.getDrugById(1);
+		
 		ms.create(1, DatabaseHandler.patientService.getPatientById(1), "url", LocalDateTime.now(), LocalDateTime.now(),
-				1, "5", "gram", 1);
+				drug, "5", "gram", 1);
 
 	}
 
