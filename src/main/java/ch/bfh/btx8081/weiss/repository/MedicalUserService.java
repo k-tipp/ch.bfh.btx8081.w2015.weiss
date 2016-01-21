e76e7a4c915d42011416adaeff70fddc63221ca8
k-tipp <k-tipp@users.noreply.github.com>
2016-01-21 21:25:01 +0100

    added comment to medical user service

--- src/main/java/ch/bfh/btx8081/weiss/repository/MedicalUserService.java
+++ src/main/java/ch/bfh/btx8081/weiss/repository/MedicalUserService.java
@@ -12,12 +12,35 @@ import javax.persistence.Query;
 import ch.bfh.btx8081.weiss.model.MedicalUser;
 import ch.bfh.btx8081.weiss.model.Patient;
 
+/**
+ * The Class MedicalUserService manages the CRUD functionality for the {@link MedicalUser} object.
+ */
 public class MedicalUserService {
 
+	/**
+	 * Gets the user by id.
+	 *
+	 * @param id
+	 *            the id
+	 * @return the user by id
+	 */
 	public Patient getUserById(int id) {
 		return DatabaseHandler.entityManager.find(Patient.class, id);
 	}
 
+	/**
+	 * Gets the user by username and password.
+	 *
+	 * @param username
+	 *            the username of the user
+	 * @param password
+	 *            the password of the user in clear text
+	 * @return the {@link MedicalUser} with the given username and password
+	 * @throws NoSuchAlgorithmException
+	 *             the no such algorithm exception
+	 * @throws InvalidKeySpecException
+	 *             the invalid key spec exception
+	 */
 	public MedicalUser getUserByUsernamePassword(String username, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
 		
 		// TODO Optimize by generating a random salt for each user.
@@ -40,7 +63,24 @@ public class MedicalUserService {
 		return (MedicalUser) users.get(0);
 	}
 
+	/**
+	 * The Class PasswordHash provides the functions to create SHA256 hashes.
+	 */
 	private static class PasswordHash {
+		
+		/**
+		 * Creates the hash for a given password and salt.
+		 *
+		 * @param password
+		 *            the password
+		 * @param salt
+		 *            the salt
+		 * @return the string
+		 * @throws NoSuchAlgorithmException
+		 *             the no such algorithm exception
+		 * @throws InvalidKeySpecException
+		 *             the invalid key spec exception
+		 */
 		private static String createHash(String password, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
 
 			byte[] hash = getPBKDF2Hash(password.toCharArray(), salt.getBytes(), 1000, 24);
@@ -49,11 +89,20 @@ public class MedicalUserService {
 
 		/**
 		 * Get the PBKDF2 hash of a password.
-		 * @param password the password to hash.
-		 * @param salt the salt
-		 * @param iterations the iteration count
-		 * @param bytes the length of the hash
+		 *
+		 * @param password
+		 *            the password to hash.
+		 * @param salt
+		 *            the salt
+		 * @param iterations
+		 *            the iteration count
+		 * @param bytes
+		 *            the length of the hash
 		 * @return the PBDKF2 hash of the password
+		 * @throws NoSuchAlgorithmException
+		 *             the no such algorithm exception
+		 * @throws InvalidKeySpecException
+		 *             the invalid key spec exception
 		 */
 		private static byte[] getPBKDF2Hash(char[] password, byte[] salt, int iterations, int bytes)
 				throws NoSuchAlgorithmException, InvalidKeySpecException {
